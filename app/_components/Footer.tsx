c8c920f62f63ce3678d718d2f0ce5fabda6deb8d
KpryKorn <roffinisacha@gmail.com>
2023-10-24 23:40:52 +0200

    style: updated UI

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
     <footer className="text-center my-6 md:my-12">
       <div className="sm:flex sm:items-center sm:justify-between">
         <Link href={"/"} className="flex items-center mb-4 sm:mb-0">
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-xl font-semibold whitespace-nowrap">
             Utopiaz
           </span>
         </Link>
-        <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+        <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
           <li>
             <Link
               href={"/politique-de-confidentialite"}
@@ -33,8 +33,8 @@ export default function Footer() {
           </li>
         </ul>
       </div>
-      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <hr className="my-6 border-gray-200 sm:mx-auto" />
+      <span className="block text-sm text-gray-500 sm:text-center">
         © 2023 Utopiaz 👨‍💻. All rights reserved.
       </span>
     </footer>
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,32 +1,11 @@
 "use client";
 
-import Link from "next/link";
-
 export default function Header() {
-  const routes = [
-    {
-      name: "Accueil",
-      path: "/",
-    },
-    {
-      name: "Blog",
-      path: "/blog",
-    },
-  ];
-
   return (
-    <header className="my-6 md:my-12 font-semibold uppercase">
-      <nav>
-        <ul className="flex gap-4 justify-center items-center">
-          {routes.map((route) => {
-            return (
-              <li key={route.name}>
-                <Link href={route.path}>{route.name}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+    <header className="my-4 md:my-8 font-bold text-xl uppercase">
+      <span className="bg-gradient-to-l from-purple-600 to-blue-500 text-transparent bg-clip-text">
+        Utopiaz
+      </span>
     </header>
   );
 }
