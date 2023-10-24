02d8e12df072a2395c9dfa4061b70e32dc7a5a52
KpryKorn <roffinisacha@gmail.com>
2023-10-24 16:11:45 +0200

    feat: add formatting date function

---
+++ app/_lib/utils.ts
@@ -0,0 +1,14 @@
+/**
+ * Formats a date string into a DD/MM/YYYY HH:mm format.
+ * @param dateString - The date string to format.
+ * @returns The formatted date string.
+ */
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear().toString();
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
