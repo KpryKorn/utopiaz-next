4c819b1de6ca83009a7bc67302686aa61b34a187
KpryKorn <roffinisacha@gmail.com>
2023-10-25 15:35:50 +0200

    fix: removed hours & minutes from format

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -10,5 +10,5 @@ export function formatDate(dateString: string): string {
   const year = date.getFullYear().toString();
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
-  return `${day}/${month}/${year} ${hours}:${minutes}`;
+  return `${day}/${month}/${year}`;
 }
