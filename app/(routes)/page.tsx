cbe42f246f782c1e8aa2c5f7cec45a42666d1ed6
KpryKorn <roffinisacha@gmail.com>
2023-10-25 10:48:14 +0200

    feat: add sliding text component
    Added a sliding text component that shows text updating every 2.5s

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,14 +1,18 @@
 import { getArticles } from "@/sanity/sanity-utils";
 import ArticlesGridItem from "../_components/ArticlesGridItem";
+import SlidingText from "../_components/SlidingText";
 
 export default async function Home() {
   const articles = await getArticles();
 
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {articles.map((article) => {
-        return <ArticlesGridItem article={article} key={article._id} />;
-      })}
-    </ul>
+    <>
+      <SlidingText text="Trusted by the most innovative minds in" />
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {articles.map((article) => {
+          return <ArticlesGridItem article={article} key={article._id} />;
+        })}
+      </ul>
+    </>
   );
 }
