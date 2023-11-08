87f095c410783f92317f58be9ea672875020e838
KpryKorn <roffinisacha@gmail.com>
2023-11-08 14:16:06 +0100

    misc: updated radio input to link components to the correct category

--- app/_components/CategoryNavbar.tsx
+++ app/_components/CategoryNavbar.tsx
@@ -1,8 +1,5 @@
-// TODO: chargement de la page => récupérer les catégories (fetch)
-// TODO: par défaut : "tous" coché => montre tous les articles
-// TODO: au clic sur une catégorie => montre les articles de cette catégorie
-
 import { getCategories } from "@/sanity/sanity-utils";
+import Link from "next/link";
 
 export default async function CategoryNavbar() {
   const categories = await getCategories();
@@ -10,41 +7,18 @@ export default async function CategoryNavbar() {
   return (
     <nav className="my-6 md-my-12 py-4 border-t border-b border-gray-400">
       <ul className="flex items-center justify-center gap-2 text-sm">
-        <li className="radio-label flex items-center justify-center border border-slate-600 rounded-full cursor-pointer">
-          <input
-            type="radio"
-            name="category"
-            value="tous"
-            id="tous"
-            defaultChecked
-            className="hidden"
-          />
-          <label
-            className="cursor-pointer px-4 py-1.5 rounded-full"
-            htmlFor="tous"
-          >
-            Tous
-          </label>
-        </li>
         {categories.map((category) => {
           return (
             <li
               key={category.titre}
-              className="radio-label flex items-center justify-center border border-slate-600 rounded-full cursor-pointer"
+              className="flex items-center justify-center rounded-full cursor-pointer"
             >
-              <input
-                type="radio"
-                name="category"
-                value={category.titre}
-                id={category.titre}
-                className="hidden"
-              />
-              <label
-                className="cursor-pointer px-4 py-1.5 rounded-full"
-                htmlFor={category.titre}
+              <Link
+                href={`/category/${category.slug}`}
+                className="px-3.5 py-1.5 bg-slate-800 border border-slate-600 rounded-full hover:bg-slate-700 transition-colors"
               >
                 {category.titre}
-              </label>
+              </Link>
             </li>
           );
         })}
